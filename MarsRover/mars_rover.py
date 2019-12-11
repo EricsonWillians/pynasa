091bb1cd9e33bd049f892ff331fd6d76b6a4821e
Ericson Willians <ericson.willians@concil.com.br>
2019-12-11 17:33:53 -0300

    Improved constructor

--- MarsRover/mars_rover.py
+++ MarsRover/mars_rover.py
@@ -5,13 +5,36 @@ import os
 import errno
 from PIL import Image
 
+
 class MarsRover:
 
     MARS_PHOTOS_URL = "https://api.nasa.gov/mars-photos/api/v1/rovers"
+    ROVER_LIST = [
+        "Curiousity",
+        "Opportunity",
+        "Spirit"
+    ]
+    CAMERA_LIST = [
+        "FHAZ",
+        "RHAZ",
+        "MAST",
+        "CHEMCAM",
+        "MAHLI",
+        "MARDI",
+        "NAVCAM",
+        "PANCAM",
+        "MINITES"
+    ]
 
-    def __init__(self, rover="curiosity", camera="fhaz"):
+    def __init__(self,
+                 rover=MarsRover.ROVER_LIST[0], 
+                 sol=1, camera=[MarsRover.CAMERA_LIST[0]], api_key="DEMO_KEY"):
         self.rover = rover
+        self.sol = sol,
         self.camera = camera
+        self.api_key = api_key
+
+    
 
 if __name__ == "__main__":
     pass
