7a53120a2225379543e7526c56fc2af92673d260
Ericson Willians <ericson.willians@concil.com.br>
2019-12-12 15:44:57 -0300

    Fixed sol bug

--- MarsRover/mars_rover.py
+++ MarsRover/mars_rover.py
@@ -30,13 +30,14 @@ class MarsRover:
                  rover=ROVER_LIST[0], 
                  sol=1, camera=CAMERA_LIST[0], api_key="DEMO_KEY"):
         self.rover = rover.lower()
-        self.sol = str(sol),
+        self.sol = sol
         self.camera = camera.lower()
         self.api_key = api_key
         self.json_data = self.fetch_data()
 
     def fetch_data(self):
-        url = f"{self.MARS_PHOTOS_URL}/{self.rover}/photos?sol={self.sol}&camera={self.camera}&api_key={self.api_key}"
+        url = f"{self.MARS_PHOTOS_URL}/{self.rover}/photos?sol={self.sol}&page=1&camera={self.camera}&api_key={self.api_key}"
+        print(f"URL: {url}")
         response = requests.get(url)
         json_data = json.loads(response.text)
         return json_data
@@ -46,7 +47,7 @@ class MarsRover:
         try:
             for photo in self.json_data["photos"]:
                 photos.append(photo["img_src"])
-        except Exception as e:
+        except Exception:
             print("ERROR: It wasn't possible to download the photos.")
             print("The response was: ")
             self.print_data()
@@ -54,10 +55,9 @@ class MarsRover:
         return photos
 
     def save_links(self):
-        count = 0
         file_dir = os.path.dirname(os.path.realpath(__file__))
         for link in self.get_links():
-            filename = f"{file_dir}/out/{self.rover}_{count}"
+            filename = f"{file_dir}/out/{self.rover.upper()}_{link.split('/')[-1]}"
             if not os.path.exists(os.path.dirname(filename)):
                 try:
                     os.makedirs(os.path.dirname(filename))
@@ -68,7 +68,6 @@ class MarsRover:
             with open(filename, 'wb') as out_file:
                 shutil.copyfileobj(response.raw, out_file)
             del response
-            count += 1
 
     def print_data(self):
         print(json.dumps(self.fetch_data(), indent=4, sort_keys=True))
--- rover_photo_downloader.py
+++ rover_photo_downloader.py
@@ -15,6 +15,9 @@ if __name__ == "__main__":
     parser.add_argument('--list-cameras', action='store_true', help="""
         List all available cameras.
     """)
+    parser.add_argument('--debug', action='store_true', help="""
+        Print API response.
+    """)
     parser.add_argument('--rover', 
         action="store", dest="rover", type=str, default="Curiosity", help="""
             Specify the motor vehicle on the surface of Mars.
@@ -25,7 +28,7 @@ if __name__ == "__main__":
     parser.add_argument('--camera', action="store", dest="camera", default="FHAZ", type=str, help="""
         Specify the camera with which the photos were taken.
     """)
-    parser.add_argument('--key', action="store", dest="key", type=str, help="""
+    parser.add_argument('--key', action="store", dest="key", default="DEMO_KEY", type=str, help="""
         Specify your NASA public api key.
     """)
 
@@ -33,7 +36,11 @@ if __name__ == "__main__":
     print_list = lambda l: [print(x) for x in l]
 
     rover = parsed_args.rover
+    if type(rover) == tuple:
+        rover = rover[0]
     sol = parsed_args.sol
+    if type(sol) == tuple:
+        sol = sol[0]
     camera = parsed_args.camera,
     if type(camera) == tuple:
         camera = camera[0]
@@ -57,3 +64,6 @@ if __name__ == "__main__":
 
     print("Done!")
     print("Go to './MarsRover/out' to view the files.")
+
+    if parsed_args.debug:
+        mars_rover.print_data()
